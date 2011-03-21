7f7358b94dfb6a3f79da09730327cbe34ccb5c75
Alvaro Fides <alfiva@itaca.upv.es>
2011-03-21 09:20:08 +0000

    Formatted as Java conventions (ref.D2.3A)

--- ctxt.che/src/main/java/org/universAAL/context/che/database/Backend.java
+++ ctxt.che/src/main/java/org/universAAL/context/che/database/Backend.java
@@ -1,5 +1,5 @@
 /*
-	Copyright 2008-2014 ITACA-TSB, http://www.tsb.upv.es
+	Copyright 2008-2010 ITACA-TSB, http://www.tsb.upv.es
 	Instituto Tecnologico de Aplicaciones de Comunicacion 
 	Avanzadas - Grupo Tecnologias para la Salud y el 
 	Bienestar (TSB)
@@ -21,199 +21,199 @@
  */
 package org.universAAL.context.che.database;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
-import org.openrdf.repository.RepositoryException;
-import org.openrdf.rio.RDFParseException;
 import org.universAAL.middleware.context.ContextEvent;
 import org.universAAL.middleware.context.owl.ContextProvider;
-import org.universAAL.middleware.serialization.MessageContentSerializer;
 
 /**
- * Interface that represents a store back end where the context history is
- * stored.
- * 
  * @author <a href="mailto:alfiva@itaca.upv.es">Alvaro Fides Valero</a>
  * 
+ *        Interface that represents a database back end where the context
+ *         history is stored.
  */
 public interface Backend {
 
     /**
-     * Establishes the connection to the store.
-     */
-    void connect();
-
-    /**
-     * Closes the connection to the store.
-     */
-    void close();
-
-    /**
-     * Stores a {@link org.universAAL.middleware.context.ContextEvent} in the
-     * underlying store.
+     * Stores a {@link org.persona.middleware.context.ContextEvent} in the
+     * underlying database.
      * 
      * @param e
      *            The context event to be stored.
      */
-    void storeEvent(ContextEvent e);
+    public void storeEvent(ContextEvent e);
 
+    // Not used in current version
     /**
-     * Retrieves a list of
-     * {@link org.universAAL.middleware.context.ContextEvent} from the
-     * underlying store, which members are the context events that match the
-     * parameters passed. A parameter can be passed a value of <code>null</code>
-     * for wildcarding.
+     * Retrieves a list of {@link org.persona.middleware.context.ContextEvent}
+     * from the underlying database, which members are the context events that
+     * match the parameters passed. A parameter can be passed a value of
+     * <code>null</code> for wildcarding.
      * <p>
      * This is not used in the current version. Will probably be deprecated.
      * 
-     * @param subject
+     * @param sub
      *            The URI of the subject of the event to be matched
-     * @param subjecttype
+     * @param subType
      *            The URI of the type of subject of the event to be matched
-     * @param predicate
+     * @param pred
      *            The URI of the predicate of the event to be matched
-     * @param object
+     * @param obj
      *            The Object of the event to be matched. It depends on the
      *            implementer of this method how to match the object.
-     * @param confidence
+     * @param acc
+     *            The accuracy of the event to be matched
+     * @param conf
      *            The confidence of the event to be matched
-     * @param expiration
+     * @param ex
      *            The expiration time of the event to be matched
-     * @param provider
+     * @param cp
      *            The context provider of the event to be matched
      * @param tstamp
      *            The timestamp in milliseconds of the event to be matched
      * @return The list of context events that matched the values passed as
      *         parameters.
      */
-    ArrayList retrieveEvent(String subject, String subjecttype,
-	    String predicate, Object object, Integer confidence,
-	    Long expiration, Object provider, Long tstamp);
+    public ArrayList retrieveEvent(String sub, String subType, String pred,
+	    Object obj, Integer acc, Integer conf, Long ex, Object cp,
+	    Long tstamp);
 
     /**
-     * Retrieves a list of
-     * {@link org.universAAL.middleware.context.ContextEvent} from the
-     * underlying store, which members are the context events that match the
-     * parameters passed, and were received after the specified timestamp. A
-     * parameter can be passed a value of <code>null</code> for wildcarding.
+     * Retrieves a list of {@link org.persona.middleware.context.ContextEvent}
+     * from the underlying database, which members are the context events that
+     * match the parameters passed, and were received after the specified
+     * timestamp. A parameter can be passed a value of <code>null</code> for
+     * wildcarding.
      * 
-     * @param subject
+     * @param sub
      *            The URI of the subject of the event to be matched
-     * @param subjecttype
+     * @param typ
      *            The URI of the type of subject of the event to be matched
-     * @param predicate
+     * @param pre
      *            The URI of the predicate of the event to be matched
-     * @param object
+     * @param obj
      *            The Object of the event to be matched. It depends on the
      *            implementer of this method how to match the object.
-     * @param confidence
+     * @param acc
+     *            The accuracy of the event to be matched
+     * @param con
      *            The confidence of the event to be matched
-     * @param expiration
+     * @param exp
      *            The expiration time of the event to be matched
-     * @param provider
+     * @param cop
      *            The context provider of the event to be matched
-     * @param tstamp
+     * @param tst
      *            The timestamp in milliseconds of the event to be matched. If
      *            not <code>null</code>, only events with this specified
      *            timestamp will be returned, as long as it is inside the time
      *            range specified by <code>tstinputfrom</code>
-     * @param tstfrom
+     * @param tstinputfrom
      *            The timestamp in milliseconds from which events are requested.
      *            Only events stored after this timestamp will be returned.
      * @return The list of context events that matched the values passed as
      *         parameters.
      */
-    ArrayList retrieveEventsFromTstmp(String subject,
-	    String subjecttype, String predicate, Object object,
-	    Integer confidence, Long expiration, ContextProvider provider,
-	    Long tstamp, Long tstfrom);
+    public ArrayList retrieveEventsFromTstmp(String sub, String typ,
+	    String pre, Object obj, Integer acc, Integer con, Long exp,
+	    ContextProvider cop, Long tst, Long tstinputfrom);
 
     /**
-     * Retrieves a list of
-     * {@link org.universAAL.middleware.context.ContextEvent} from the
-     * underlying store, which members are the context events that match the
-     * parameters passed, and were received before the specified timestamp. A
-     * parameter can be passed a value of <code>null</code> for wildcarding.
+     * Retrieves a list of {@link org.persona.middleware.context.ContextEvent}
+     * from the underlying database, which members are the context events that
+     * match the parameters passed, and were received before the specified
+     * timestamp. A parameter can be passed a value of <code>null</code> for
+     * wildcarding.
      * 
-     * @param subject
+     * @param sub
      *            The URI of the subject of the event to be matched
-     * @param subjecttype
+     * @param typ
      *            The URI of the type of subject of the event to be matched
-     * @param predicate
+     * @param pre
      *            The URI of the predicate of the event to be matched
-     * @param object
+     * @param obj
      *            The Object of the event to be matched. It depends on the
      *            implementer of this method how to match the object.
-
-     * @param confidence
+     * @param acc
+     *            The accuracy of the event to be matched
+     * @param con
      *            The confidence of the event to be matched
-     * @param expiration
+     * @param exp
      *            The expiration time of the event to be matched
-     * @param provider
+     * @param cop
      *            The context provider of the event to be matched
-     * @param tstamp
+     * @param tst
      *            The timestamp in milliseconds of the event to be matched. If
      *            not <code>null</code>, only events with this specified
      *            timestamp will be returned, as long as it is inside the time
      *            range specified by <code>tstinputto</code>
-     * @param tstto
+     * @param tstinputto
      *            The timestamp in milliseconds until which events are
      *            requested. Only events stored before this timestamp will be
      *            returned.
      * @return The list of context events that matched the values passed as
      *         parameters.
      */
-    ArrayList retrieveEventsToTstmp(String subject, String subjecttype,
-	    String predicate, Object object, Integer confidence,
-	    Long expiration, ContextProvider provider, Long tstamp, Long tstto);
+    public ArrayList retrieveEventsToTstmp(String sub, String typ, String pre,
+	    Object obj, Integer acc, Integer con, Long exp,
+	    ContextProvider cop, Long tst, Long tstinputto);
 
     /**
-     * Retrieves a list of
-     * {@link org.universAAL.middleware.context.ContextEvent} from the
-     * underlying store, which members are the context events that match the
-     * parameters passed, and were received within the specified time range. A
-     * parameter can be passed a value of <code>null</code> for wildcarding.
+     * Retrieves a list of {@link org.persona.middleware.context.ContextEvent}
+     * from the underlying database, which members are the context events that
+     * match the parameters passed, and were received within the specified time
+     * range. A parameter can be passed a value of <code>null</code> for
+     * wildcarding.
      * 
-     * @param subject
+     * @param sub
      *            The URI of the subject of the event to be matched
-     * @param subjecttype
+     * @param typ
      *            The URI of the type of subject of the event to be matched
-     * @param predicate
+     * @param pre
      *            The URI of the predicate of the event to be matched
-     * @param object
+     * @param obj
      *            The Object of the event to be matched. It depends on the
      *            implementer of this method how to match the object.
-     * @param confidence
+     * @param acc
+     *            The accuracy of the event to be matched
+     * @param con
      *            The confidence of the event to be matched
-     * @param expiration
+     * @param exp
      *            The expiration time of the event to be matched
-     * @param provider
+     * @param cop
      *            The context provider of the event to be matched
-     * @param tstamp
+     * @param tst
      *            The timestamp in milliseconds of the event to be matched. If
      *            not <code>null</code>, only events with this specified
      *            timestamp will be returned, as long as it is inside the time
      *            range
-     * @param tstfrom
+     * @param tstinputfrom
      *            The timestamp in milliseconds from which events are requested.
      *            Only events stored after this timestamp will be returned.
-     * @param tstto
+     * @param tstinputto
      *            The timestamp in milliseconds until which events are
      *            requested. Only events stored before this timestamp will be
      *            returned.
      * @return The list of context events that matched the values passed as
      *         parameters.
      */
-    ArrayList retrieveEventsBetweenTstmp(String subject,
-	    String subjecttype, String predicate, Object object,
-	    Integer confidence, Long expiration, ContextProvider provider,
-	    Long tstamp, Long tstfrom, Long tstto);
+    public ArrayList retrieveEventsBetweenTstmp(String sub, String typ,
+	    String pre, Object obj, Integer acc, Integer con, Long exp,
+	    ContextProvider cop, Long tst, Long tstinputfrom, Long tstinputto);
 
     /**
-     * Retrieves a list of
-     * {@link org.universAAL.middleware.context.ContextEvent} from the
-     * underlying store, as a result of a SPARQL query.
+     * Returns the result of a SPARQL query issued to the underlying database.
+     * 
+     * @param input
+     *            The SPARQL query to be executed. All types of SPARQL queries
+     *            are allowed.
+     * @return The serialized form of the model resulting from the execution of
+     *         the query, in RDF/XML-ABBREV format.
+     */
+    public String queryBySPARQL(String input);
+
+    /**
+     * Retrieves a list of {@link org.persona.middleware.context.ContextEvent}
+     * from the underlying database, as a result of a SPARQL query.
      * 
      * @param input
      *            The SPARQL query that defines the events to be returned. The
@@ -222,67 +222,26 @@ public interface Backend {
      *            SELECT queries are allowed.
      * @return The list of context events that matched the query.
      */
-    ArrayList retrieveEventsBySPARQL(String input);
+    public ArrayList retrieveEventsBySPARQL(String input);
 
     /**
-     * Returns the result of a SPARQL query issued to the underlying store.
+     * Removes all events from the underlying database that were received until
+     * the specified timestamp.
      * 
-     * The types of SPARQL queries allowed depend on the underlying
-     * implementation, but these are mandated to include at least SELECT and
-     * DESCRIBE, and encouraged to also allow CONSTRUCT and ASK. SPARQL-Update
-     * is optional. Implementations should inform in case an unsupported query
-     * is received.
-     * 
-     * The underlying implementations should return results in RDF/XML-ABBREV
-     * format encoded in UTF-8 in case of RDF models, and also if possible in
-     * case of lists of bindings for SELECT queries. Results for ASK and UPDATE
-     * queries (if implemented) should follow the format \"true\" or \"false\"
-     * Strings in case of ASK and \"true\" in case of a successful UPDATE query.
-     * 
-     * @param input
-     *            The SPARQL query to be executed.
-     * @return The serialized form of the result from the execution of the
-     *         query.
-     */
-    String queryBySPARQL(String input);
-
-    /**
-     * Removes all events from the underlying store that were received until the
-     * specified timestamp.
-     * 
-     * @param tstamp
+     * @param tst
      *            The timestamp in milliseconds until which events are removed.
      *            A value of 0 does nothing.
      */
-    void removeOldEvents(long tstamp);
+    public void removeOldEvents(long tst);
 
     /**
-     * Set the MessageContentSerializer universAAL Parser that can be used to
-     * serialize and parse RDF data to universAAL data and vice-versa. This
-     * might be helpful in many implementations, and allows the activator of the
-     * CHE to look for such service in OSGi, which should be provided by the
-     * mandatory component mw.data.serialization.
-     * 
-     * There is no guarantee though that the parser is set before it is used in
-     * the implementation, so check for nulls. If an implementation of a Backend
-     * does not need to use such parser, leave its implementation of this method
-     * empty.
-     * 
-     * @param service
-     *            The MessageContentSerializer service implementation found in
-     *            OSGi
+     * Closes the connection to the database
      */
-    void setuAALParser(MessageContentSerializer service);
+    public void close();
 
     /**
-     * Fills the initial store with the OWL data of the ontologies from the OWL
-     * files in the config folder (or registered in the system). Also used for
-     * updates when new ontologies are installed.
-     * 
-     * @throws RepositoryException
-     * @throws RDFParseException
-     * @throws IOException
+     * Establishes the connection to the database
      */
-    public void populate() throws RepositoryException, RDFParseException,
-	    IOException;
+    public void connect();
+
 }

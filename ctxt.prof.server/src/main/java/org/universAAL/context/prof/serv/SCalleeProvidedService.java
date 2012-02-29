2e45aacc65a0bb3e98308c1ee3d0a183a89a2012
Alvaro Fides <alfiva@itaca.upv.es>
2012-02-29 12:14:01 +0000

    Added licenses, test and javadoc

--- ctxt.prof.server/src/main/java/org/universAAL/context/prof/serv/SCalleeProvidedService.java
+++ ctxt.prof.server/src/main/java/org/universAAL/context/prof/serv/SCalleeProvidedService.java
@@ -1,3 +1,24 @@
+/*
+	Copyright 2012-2014 ITACA-TSB, http://www.tsb.upv.es
+	Instituto Tecnologico de Aplicaciones de Comunicacion 
+	Avanzadas - Grupo Tecnologias para la Salud y el 
+	Bienestar (TSB)
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
 package org.universAAL.context.prof.serv;
 
 import org.universAAL.middleware.owl.ManagedIndividual;
@@ -14,12 +35,25 @@ import org.universAAL.ontology.profile.Profilable;
 import org.universAAL.ontology.profile.Profile;
 import org.universAAL.ontology.profile.service.ProfilingService;
 
+/**
+ * The class where all service profiles are built.
+ * 
+ * @author alfiva
+ * 
+ */
 public class SCalleeProvidedService extends ProfilingService {
 
-    public static final String NAMESPACE = "http://ontology.universAAL.org/ProfilingServer.owl#";
-    public static final String MY_URI = NAMESPACE+"ProvidedServices";
+    /**
+     * Default namespace root. Must be the same as in SCallee.
+     */
+    public static final String NAMESPACE = SCallee.NAMESPACE;
+    /**
+     * Ontological URI of the class (because it extends and ontology class>
+     * ProfilingService)
+     */
+    public static final String MY_URI = NAMESPACE + "ProvidedServices";
     // VALUES FOR STATIC NON-TYPICAL SERVICE PROFILES
-    public static final String FAKE_URI = NAMESPACE+"placeholder";
+    public static final String FAKE_URI = NAMESPACE + "placeholder";
     protected static final String ADD_PROFILE = NAMESPACE + "servA";
     protected static final String CHANGE_PROFILE = NAMESPACE + "servB";
     protected static final String GET_USERS = NAMESPACE + "servC";
@@ -42,11 +76,19 @@ public class SCalleeProvidedService extends ProfilingService {
     public static final String IN_CHANGE = "inputEditorChange";
     public static final String IN_REMOVE = "inputEditorRemove";
 
+    /**
+     * Default constructor.
+     * 
+     * @param uri Instance URI
+     */
     public SCalleeProvidedService(String uri) {
 	super(uri);
     }
 
-    public static ServiceProfile[] profiles=new ServiceProfile[4];
+    /**
+     * Where the service profiles are stored.
+     */
+    public static ServiceProfile[] profiles = new ServiceProfile[4];
 
     static {
 	OntologyManagement.getInstance().register(
@@ -57,87 +99,121 @@ public class SCalleeProvidedService extends ProfilingService {
 				return new SCalleeProvidedService(instanceURI);
 			    }
 			}));
-	
-	//STATIC NON-TYPICAL SERVICE PROFILES
-	
+
+	// STATIC NON-TYPICAL SERVICE PROFILES
+
 	// GET_PROFILE(Profilable) -> Profile
-	// Returns the profile (with all its stored properties) associated to the profile whose URI matches the one of the passed parameter
+	// Returns the profile (with all its stored properties) associated to
+	// the profile whose URI matches the one of the passed parameter
 
 	// ADD_PROFILE(Profilable, Profile) -> void
 	// Adds the passed profile to the store, with all its properties (that
 	// can be serialized), and associates it to the given profilable.
 	// Currently works like CHANGE PROFILE, but does not remove previous
 	// value of an associated profile to the profilable, if any
-	Service prof6=new ProfilingService(ADD_PROFILE);
+	Service prof6 = new ProfilingService(ADD_PROFILE);
 	ProcessInput input1 = new ProcessInput(INPUT_ADD_PROFILE);
 	input1.setParameterType(ManagedIndividual.getTypeURI(Profilable.MY_URI));
 	MergedRestriction restr1 = MergedRestriction.getFixedValueRestriction(
 		ProfilingService.PROP_CONTROLS, input1.asVariableReference());
-	prof6.addInstanceLevelRestriction(restr1, new String[]{ProfilingService.PROP_CONTROLS});
+	prof6.addInstanceLevelRestriction(restr1,
+		new String[] { ProfilingService.PROP_CONTROLS });
 	prof6.getProfile().addInput(input1);
 	ProcessInput input2 = new ProcessInput(INPUT2_ADD_PROFILE);
 	input2.setParameterType(ManagedIndividual.getTypeURI(Profile.MY_URI));
 	prof6.getProfile().addInput(input2);
-	prof6.getProfile().addAddEffect(new String[]{ProfilingService.PROP_CONTROLS,Profilable.PROP_HAS_PROFILE},
-	input2.asVariableReference());
+	prof6.getProfile().addAddEffect(
+		new String[] { ProfilingService.PROP_CONTROLS,
+			Profilable.PROP_HAS_PROFILE },
+		input2.asVariableReference());
 	profiles[0] = prof6.getProfile();
-	
+
 	// CHANGE_PROFILE(Profilable, Profile) -> void
 	// Removes ALL REFERENCE to the previous profile of the passed
 	// profilable, and then adds the new profile with its properties. The
 	// properties of old profile (including subprofiles) are not saved,
 	// although their values are.
-	Service prof7=new ProfilingService(CHANGE_PROFILE);
+	Service prof7 = new ProfilingService(CHANGE_PROFILE);
 	ProcessInput input3 = new ProcessInput(INPUT_CHANGE_PROFILE);
 	input3.setParameterType(ManagedIndividual.getTypeURI(Profilable.MY_URI));
 	MergedRestriction restr2 = MergedRestriction.getFixedValueRestriction(
 		ProfilingService.PROP_CONTROLS, input3.asVariableReference());
-	prof7.addInstanceLevelRestriction(restr2, new String[]{ProfilingService.PROP_CONTROLS});
+	prof7.addInstanceLevelRestriction(restr2,
+		new String[] { ProfilingService.PROP_CONTROLS });
 	prof7.getProfile().addInput(input3);
 	ProcessInput input4 = new ProcessInput(INPUT2_CHANGE_PROFILE);
 	input4.setParameterType(ManagedIndividual.getTypeURI(Profile.MY_URI));
 	prof7.getProfile().addInput(input4);
-	prof7.getProfile().addChangeEffect(new String[]{ProfilingService.PROP_CONTROLS,Profilable.PROP_HAS_PROFILE},
+	prof7.getProfile().addChangeEffect(
+		new String[] { ProfilingService.PROP_CONTROLS,
+			Profilable.PROP_HAS_PROFILE },
 		input4.asVariableReference());
 	profiles[1] = prof7.getProfile();
-	
+
 	// GET_USERS() -> [User]
 	// Not working yet
-	Service prof9=new ProfilingService(GET_USERS);
-//	prof9.put(Path.at(ProfilingService.PROP_CONTROLS), Arg.out(new Profilable(FAKE_URI)), OUTPUT_GET_USERS);
+	Service prof9 = new ProfilingService(GET_USERS);
+	// prof9.put(Path.at(ProfilingService.PROP_CONTROLS), Arg.out(new
+	// Profilable(FAKE_URI)), OUTPUT_GET_USERS);
 	ProcessOutput output1 = new ProcessOutput(OUTPUT_GET_USERS);
 	output1.setParameterType(Profilable.MY_URI);
 	prof9.getProfile().addOutput(output1);
-	prof9.getProfile().addSimpleOutputBinding(output1, new String[]{ProfilingService.PROP_CONTROLS});
+	prof9.getProfile().addSimpleOutputBinding(output1,
+		new String[] { ProfilingService.PROP_CONTROLS });
 	profiles[2] = prof9.getProfile();
-	
+
 	// GET_SUBPROFILES(Profile) -> [Subprofile]
 	// Not working yet
-	Service prof10=new ProfilingService(GET_SUBPROFILES);
+	Service prof10 = new ProfilingService(GET_SUBPROFILES);
 	ProcessInput input5 = new ProcessInput(INPUT_GET_SUBPROFILES);
 	input5.setParameterType(ManagedIndividual.getTypeURI(Profile.MY_URI));
 	MergedRestriction restr3 = MergedRestriction.getFixedValueRestriction(
 		Profilable.PROP_HAS_PROFILE, input5.asVariableReference());
-	prof10.addInstanceLevelRestriction(restr3, new String[]{ProfilingService.PROP_CONTROLS,Profilable.PROP_HAS_PROFILE});
+	prof10.addInstanceLevelRestriction(restr3, new String[] {
+		ProfilingService.PROP_CONTROLS, Profilable.PROP_HAS_PROFILE });
 	prof10.getProfile().addInput(input5);
 	ProcessOutput output3 = new ProcessOutput(OUT_GET_SUBPROFILES);
 	output3.setParameterType(Profile.MY_URI);
 	prof10.getProfile().addOutput(output3);
-	prof10.getProfile().addSimpleOutputBinding(output3, new String[]{ProfilingService.PROP_CONTROLS,Profilable.PROP_HAS_PROFILE,Profile.PROP_HAS_SUB_PROFILE});
+	prof10.getProfile().addSimpleOutputBinding(
+		output3,
+		new String[] { ProfilingService.PROP_CONTROLS,
+			Profilable.PROP_HAS_PROFILE,
+			Profile.PROP_HAS_SUB_PROFILE });
 	profiles[3] = prof10.getProfile();
-	
+
     }
-    
-    //DYNAMIC TYPICAL SERVICE PROFILES
+
+    // DYNAMIC TYPICAL SERVICE PROFILES
+    /**
+     * Gives you the 4 typical service profiles of an editor service: Get, Add,
+     * Change and Remove. When handling requests in you Callee, you can use the
+     * references to services and arguments URIs prepending
+     * <code>namespace</code> to SimpleEditor constants.
+     * 
+     * @param namespace
+     *            The namespace of your server, ending with the character #. You
+     *            can optionally add some prefix after the # if you use
+     *            SimpleEditor more than once in the same Callee.
+     * @param ontologyURI
+     *            The MY_URI of the class of Service ontology you are going to
+     *            implement
+     * @param path
+     *            The property path from the root of the Service ontology
+     *            concept to the exact concept you want to manage
+     * @param editedURI
+     *            The MY_URI of the class of the concept ontology that you want
+     *            to manage, which is at the end of the property path
+     * @return An array with the 4 typical service profiles
+     */
     public static ServiceProfile[] getServiceProfiles(String namespace,
 	    String ontologyURI, String[] path, String editedURI) {
 
 	ServiceProfile[] profiles = new ServiceProfile[4];
 
 	// Get
-	Service prof1 = (Service) OntologyManagement
-		.getInstance()
-		.getResource(ontologyURI, namespace + SERVICE_GET);
+	Service prof1 = (Service) OntologyManagement.getInstance().getResource(
+		ontologyURI, namespace + SERVICE_GET);
 	ProcessInput input1 = new ProcessInput(namespace + IN_GET);
 	input1.setParameterType(editedURI);
 	input1.setCardinality(1, 1);
@@ -152,33 +228,28 @@ public class SCalleeProvidedService extends ProfilingService {
 	profiles[0] = prof1.getProfile();
 
 	// Add
-	Service prof2 = ((Service) OntologyManagement
-		.getInstance()
+	Service prof2 = ((Service) OntologyManagement.getInstance()
 		.getResource(ontologyURI, namespace + SERVICE_ADD));
 	ProcessInput input2 = new ProcessInput(namespace + IN_ADD);
 	input2.setParameterType(editedURI);
 	input2.setCardinality(1, 1);
 	prof2.getProfile().addInput(input2);
-	prof2.getProfile().addAddEffect(path,
-		input2.asVariableReference());
+	prof2.getProfile().addAddEffect(path, input2.asVariableReference());
 	profiles[1] = prof2.getProfile();
 
 	// Change
-	Service prof3 = ((Service) OntologyManagement
-		.getInstance().getResource(ontologyURI,
-			namespace + SERVICE_CHANGE));
+	Service prof3 = ((Service) OntologyManagement.getInstance()
+		.getResource(ontologyURI, namespace + SERVICE_CHANGE));
 	ProcessInput input3 = new ProcessInput(namespace + IN_CHANGE);
 	input3.setCardinality(1, 1);
 	input3.setParameterType(editedURI);
 	prof3.getProfile().addInput(input3);
-	prof3.getProfile().addChangeEffect(path,
-		input3.asVariableReference());
+	prof3.getProfile().addChangeEffect(path, input3.asVariableReference());
 	profiles[2] = prof3.getProfile();
 
 	// Remove
-	Service prof4 = ((Service) OntologyManagement
-		.getInstance().getResource(ontologyURI,
-			namespace + SERVICE_REMOVE));
+	Service prof4 = ((Service) OntologyManagement.getInstance()
+		.getResource(ontologyURI, namespace + SERVICE_REMOVE));
 	ProcessInput input4 = new ProcessInput(namespace + IN_REMOVE);
 	input4.setParameterType(editedURI);
 	input4.setCardinality(1, 1);

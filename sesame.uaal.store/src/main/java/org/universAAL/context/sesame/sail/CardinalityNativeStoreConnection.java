30bfccf79cc00c420536d62f50d929d7ddf0459e
Alvaro Fides <alfiva@itaca.upv.es>
2012-10-03 08:00:00 +0000

    Added a second cardinality check for exact cardinality: 1, thus fixing a bug when stored ontologies used setFunctional (=exact cardinality: 1)

--- sesame.uaal.store/src/main/java/org/universAAL/context/sesame/sail/CardinalityNativeStoreConnection.java
+++ sesame.uaal.store/src/main/java/org/universAAL/context/sesame/sail/CardinalityNativeStoreConnection.java
@@ -358,29 +358,17 @@ public class CardinalityNativeStoreConnection extends NotifyingSailConnectionBas
 		throws SailException
 	{
 		txnLock = nativeStore.getTransactionLock();
-//	//This is old original code replaced with latest original version
-//		try {
-//			nativeStore.getTripleStore().startTransaction();
-//		}
-//		catch (IOException e) {
-//			txnLock.release();
-//			throw new SailException(e);
-//		}
-//		catch (RuntimeException e) {
-//			txnLock.release();
-//			throw e;
-//		}
-		boolean relaseLock = true;
 
 		try {
-		    nativeStore.getTripleStore().startTransaction();
-		    relaseLock = false;
-		} catch (IOException e) {
-		    throw new SailException(e);
-		} finally {
-		    if (relaseLock) {
+			nativeStore.getTripleStore().startTransaction();
+		}
+		catch (IOException e) {
 			txnLock.release();
-		    }
+			throw new SailException(e);
+		}
+		catch (RuntimeException e) {
+			txnLock.release();
+			throw e;
 		}
 	}
 
@@ -481,8 +469,8 @@ public class CardinalityNativeStoreConnection extends NotifyingSailConnectionBas
 				}
 
 				//START PATCH CARDINALITY
-				if(hasMaxCardinality1(pred, contexts) && explicit){//Property has max/Card=1 & stmt not inferrd
-				    removeStatements(subj, pred, null, true, contexts);//rem. existing values so there is only 1
+				if(hasMaxCardinality1(pred, contexts)){//Check if property has maxCard=1
+				    removeStatements(subj, pred, null, true, contexts);//remove existing values so there is only 1
 				}
 				//END PATCH CARDINALITY
 				boolean wasNew = nativeStore.getTripleStore().storeTriple(subjID, predID, objID, contextID,
@@ -538,7 +526,7 @@ public class CardinalityNativeStoreConnection extends NotifyingSailConnectionBas
 //			nonNativeInt);
 		while (subs.hasNext()) {
 		    Statement st = subs.next();
-		    // For each subject (a Restriction on "pred") check if it's
+		    // For each subject (a Restriction on "pred") check if it´s
 		    // maxCardinality and ==1
 		    if (size(st.getSubject(), maxCard, one, true, contexts) > 0 || size(st.getSubject(), exactCard, one, true, contexts) > 0)
 			return true;

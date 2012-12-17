30ad74913b08bb302c92b33e216043c704ec76ea
Alvaro Fides <alfiva@itaca.upv.es>
2012-12-17 08:30:04 +0000

    ignore list

--- ctxt.reliability.reasoner/src/main/java/org/universAAL/reliability/reasoner/ReliabilityReasonerActivator.java
+++ ctxt.reliability.reasoner/src/main/java/org/universAAL/reliability/reasoner/ReliabilityReasonerActivator.java
@@ -16,8 +16,15 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 * @author <a href="mailto:abu.sadat@uni-siegen.de">Rubaiyat Sadat</a>
-*	       Â©2012
+*	       ©2012
+*@author alviva
 */
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
@@ -26,27 +33,33 @@ import org.osgi.framework.ServiceListener;
 import org.osgi.framework.ServiceReference;
 import org.universAAL.middleware.container.ModuleContext;
 import org.universAAL.middleware.container.osgi.uAALBundleContainer;
+import org.universAAL.middleware.container.osgi.util.BundleConfigHome;
+import org.universAAL.middleware.container.utils.LogUtils;
+import org.universAAL.middleware.context.ContextEvent;
+import org.universAAL.middleware.context.ContextEventPattern;
 import org.universAAL.middleware.context.ContextPublisher;
-import org.universAAL.middleware.context.ContextSubscriber;
 import org.universAAL.middleware.context.DefaultContextPublisher;
 import org.universAAL.middleware.context.owl.ContextProvider;
 import org.universAAL.middleware.context.owl.ContextProviderType;
-import org.universAAL.middleware.serialization.MessageContentSerializerEx;
+import org.universAAL.middleware.owl.ManagedIndividual;
+import org.universAAL.middleware.owl.MergedRestriction;
+import org.universAAL.middleware.sodapop.msg.MessageContentSerializerEx;
+import org.universAAL.mini.reasoner.Activator;
+import org.universAAL.mini.reasoner.CSubsMulti;
 
 
 public class ReliabilityReasonerActivator implements BundleActivator, ServiceListener {
     public static BundleContext osgiContext = null;
     public static ModuleContext context = null;
     public static ContextPublisher cpublisher = null;
-    public static ContextSubscriber csubscriber = null;
     public static SCaller scaller = null;
     private static Object theFileLock = new Object();
-//    private static final String FILENAME = "Situations.txt";
-//    private static final String FILENAME2 = "Query.txt";
-//    private static final Object WILDCARD = "*";
-//    private static File confHome = new File(new BundleConfigHome(
-//	    "ctxt.reliability.reasoner").getAbsolutePath());
-//    public static ArrayList<CSubsMulti> subs = new ArrayList<CSubsMulti>();
+    private static final String FILENAME = "Situations.txt";
+    private static final String FILENAME2 = "Query.txt";
+    private static final Object WILDCARD = "*";
+    private static File confHome = new File(new BundleConfigHome(
+	    "ctxt.reliability.reasoner").getAbsolutePath());
+    public static ArrayList<CSubsMulti> subs = new ArrayList<CSubsMulti>();
 
     /*
      * (non-Javadoc)
@@ -56,8 +69,8 @@ public class ReliabilityReasonerActivator implements BundleActivator, ServiceLis
      * )
      */
     public void start(BundleContext bcontext) throws Exception {
-	osgiContext = bcontext;
-	context = uAALBundleContainer.THE_CONTAINER
+	Activator.osgiContext = bcontext;
+	Activator.context = uAALBundleContainer.THE_CONTAINER
 		.registerModule(new Object[] { osgiContext });
 	ContextProvider info = new ContextProvider(
 		"http://ontology.itaca.es/Reasoner.owl#ReasonerPublisher");
@@ -77,14 +90,13 @@ public class ReliabilityReasonerActivator implements BundleActivator, ServiceLis
 	}
 	
 	// Start all subscribers (heavy, do in thread)
-//	new Thread(){
-//	    @Override
-//	    public void run() {
-//		initializeSubscribers();
-//	    }
-//	}.start();
+	new Thread(){
+	    @Override
+	    public void run() {
+		initializeSubscribers();
+	    }
+	}.start();
 	
-	csubscriber=new ReliabilityReasonerSubscriber(context);
     }
 
     /*
@@ -96,123 +108,122 @@ public class ReliabilityReasonerActivator implements BundleActivator, ServiceLis
     public void stop(BundleContext arg0) throws Exception {
 	cpublisher.close();
 	scaller.close();
-//	for (CSubsMulti sub : (CSubsMulti[]) subs.toArray(new CSubsMulti[] {})) {
-//	    if (sub != null)
-//		sub.close();
-//	}
-//	subs.clear();
-	csubscriber.close();
+	for (CSubsMulti sub : (CSubsMulti[]) subs.toArray(new CSubsMulti[] {})) {
+	    if (sub != null)
+		sub.close();
+	}
+	subs.clear();
     }
 
-//    /**
-//     * Reads a file looking for "situations", described as statements just like
-//     * in the original reasoner. The format of the file must be as follows
-//     * <p>
-//     * File: Situations.txt
-//     * <p>
-//     * ID subjectURI predicateURI objectURI
-//     * <P>
-//     * ...
-//     * <p>
-//     * Predicate and object URI are optional. This statement describes loosely a
-//     * Context Event Pattern. When an event matching the pattern is received,
-//     * the file with the corresponding ID is read. The name of the file is
-//     * IDQuery.txt, being ID the id expressed in the previous file. The content
-//     * must be a SPARQL CONSTRUCT query that builds a valid ContextEvent.
-//     * 
-//     */
-//    private static void initializeSubscribers() {
-//	// TODO Improve management of situation patterns. Enhance situation
-//	// description.
-//	synchronized (getFileLock()) {
-//	    try {
-//		String readline = "";
-//		BufferedReader br = new BufferedReader(new FileReader(new File(
-//			confHome, FILENAME)));
-//		readline = br.readLine();
-//		while (readline != null) {
-//		    if (!readline.isEmpty()) {
-//			String situation[] = readline.split(" ", 4);
-//			
-//			if (situation.length > 1) {
-//			    // at least subject
-//			    ContextEventPattern cep = new ContextEventPattern();
-//			    String id = situation[0];
-//			    if (!WILDCARD.equals(situation[1])) {
-//				if (ManagedIndividual
-//					.isRegisteredClassURI(situation[1])) {
-//				    cep.addRestriction(MergedRestriction
-//					    .getAllValuesRestriction(
-//						    ContextEvent.PROP_RDF_SUBJECT,
-//						    situation[1]));
-//				} else {
-//				    cep.addRestriction(MergedRestriction
-//					    .getFixedValueRestriction(
-//						    ContextEvent.PROP_RDF_SUBJECT,
-//						    situation[1]));
-//				}
-//			    }
-//			    
-//			    if (situation.length > 2) {
-//				// at least subj & pred
-//				if (!WILDCARD.equals(situation[2])) {
-//				    cep.addRestriction(MergedRestriction
-//					    .getFixedValueRestriction(
-//						    ContextEvent.PROP_RDF_PREDICATE,
-//						    situation[2]));
-//				}
-//				
-//				if (situation.length > 3) {
-//				    // subj & pred & obj
-//				    if (!WILDCARD.equals(situation[3])) {
-//					if (ManagedIndividual
-//						.isRegisteredClassURI(situation[3])) {
-//					    cep.addRestriction(MergedRestriction
-//						    .getAllValuesRestriction(
-//							    ContextEvent.PROP_RDF_OBJECT,
-//							    situation[3]));
-//					} else {
-//					    cep.addRestriction(MergedRestriction
-//						    .getFixedValueRestriction(
-//							    ContextEvent.PROP_RDF_OBJECT,
-//							    situation[3]));
-//					}
-//				    }
-//				}
-//			    }
-//			    
-//			    // Now read the query for the situation
-//			    String query = "";
-//			    String readline2 = "";
-//			    BufferedReader br2 = new BufferedReader(
-//				    new FileReader(new File(confHome,
-//					    (id + FILENAME2))));
-//			    readline2 = br2.readLine();
-//			    while (readline2 != null) {
-//				query += readline2;
-//				readline2 = br2.readLine();
-//			    }
-//			    br2.close();
-//			    // Register a subscriber for each pair of
-//			    // situation/query
-//			    subs.add(new CSubsMulti(context,
-//				    new ContextEventPattern[] { cep }, query));
-//			}
-//		    }
-//		    readline = br.readLine();
-//		}
-//		br.close();
-//	    } catch (FileNotFoundException e) {
-//		LogUtils.logError(context, ReliabilityReasonerActivator.class,
-//			"getInitialSubscriptions",
-//			new Object[] { "Could not find file" }, e);
-//	    } catch (Exception e) {
-//		LogUtils.logError(context, ReliabilityReasonerActivator.class,
-//			"getInitialSubscriptions",
-//			new Object[] { "Could not open file" }, e);
-//	    }
-//	}
-//    }
+    /**
+     * Reads a file looking for "situations", described as statements just like
+     * in the original reasoner. The format of the file must be as follows
+     * <p>
+     * File: Situations.txt
+     * <p>
+     * ID subjectURI predicateURI objectURI
+     * <P>
+     * ...
+     * <p>
+     * Predicate and object URI are optional. This statement describes loosely a
+     * Context Event Pattern. When an event matching the pattern is received,
+     * the file with the corresponding ID is read. The name of the file is
+     * IDQuery.txt, being ID the id expressed in the previous file. The content
+     * must be a SPARQL CONSTRUCT query that builds a valid ContextEvent.
+     * 
+     */
+    private static void initializeSubscribers() {
+	// TODO Improve management of situation patterns. Enhance situation
+	// description.
+	synchronized (getFileLock()) {
+	    try {
+		String readline = "";
+		BufferedReader br = new BufferedReader(new FileReader(new File(
+			confHome, FILENAME)));
+		readline = br.readLine();
+		while (readline != null) {
+		    if (!readline.isEmpty()) {
+			String situation[] = readline.split(" ", 4);
+			
+			if (situation.length > 1) {
+			    // at least subject
+			    ContextEventPattern cep = new ContextEventPattern();
+			    String id = situation[0];
+			    if (!WILDCARD.equals(situation[1])) {
+				if (ManagedIndividual
+					.isRegisteredClassURI(situation[1])) {
+				    cep.addRestriction(MergedRestriction
+					    .getAllValuesRestriction(
+						    ContextEvent.PROP_RDF_SUBJECT,
+						    situation[1]));
+				} else {
+				    cep.addRestriction(MergedRestriction
+					    .getFixedValueRestriction(
+						    ContextEvent.PROP_RDF_SUBJECT,
+						    situation[1]));
+				}
+			    }
+			    
+			    if (situation.length > 2) {
+				// at least subj & pred
+				if (!WILDCARD.equals(situation[2])) {
+				    cep.addRestriction(MergedRestriction
+					    .getFixedValueRestriction(
+						    ContextEvent.PROP_RDF_PREDICATE,
+						    situation[2]));
+				}
+				
+				if (situation.length > 3) {
+				    // subj & pred & obj
+				    if (!WILDCARD.equals(situation[3])) {
+					if (ManagedIndividual
+						.isRegisteredClassURI(situation[3])) {
+					    cep.addRestriction(MergedRestriction
+						    .getAllValuesRestriction(
+							    ContextEvent.PROP_RDF_OBJECT,
+							    situation[3]));
+					} else {
+					    cep.addRestriction(MergedRestriction
+						    .getFixedValueRestriction(
+							    ContextEvent.PROP_RDF_OBJECT,
+							    situation[3]));
+					}
+				    }
+				}
+			    }
+			    
+			    // Now read the query for the situation
+			    String query = "";
+			    String readline2 = "";
+			    BufferedReader br2 = new BufferedReader(
+				    new FileReader(new File(confHome,
+					    (id + FILENAME2))));
+			    readline2 = br2.readLine();
+			    while (readline2 != null) {
+				query += readline2;
+				readline2 = br2.readLine();
+			    }
+			    br2.close();
+			    // Register a subscriber for each pair of
+			    // situation/query
+			    subs.add(new CSubsMulti(context,
+				    new ContextEventPattern[] { cep }, query));
+			}
+		    }
+		    readline = br.readLine();
+		}
+		br.close();
+	    } catch (FileNotFoundException e) {
+		LogUtils.logError(context, Activator.class,
+			"getInitialSubscriptions",
+			new Object[] { "Could not find file" }, e);
+	    } catch (Exception e) {
+		LogUtils.logError(context, Activator.class,
+			"getInitialSubscriptions",
+			new Object[] { "Could not open file" }, e);
+	    }
+	}
+    }
 
     /**
      * For synchronization
--- ctxt.reliability.reasoner/src/main/java/org/universAAL/reliability/reasoner/ReliabilityReasonerPublisher.java
+++ ctxt.reliability.reasoner/src/main/java/org/universAAL/reliability/reasoner/ReliabilityReasonerPublisher.java
@@ -2,25 +2,26 @@ package org.universAAL.reliability.reasoner;
 
 /**Copyright [2011-2014] [University of Siegen, Embedded System Instiute]
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
- http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
- * @author <a href="mailto:abu.sadat@uni-siegen.de">Rubaiyat Sadat</a>
- *	       Â©2012
- */
+* @author <a href="mailto:abu.sadat@uni-siegen.de">Rubaiyat Sadat</a>
+*	       ©2012
+*/
 import org.universAAL.middleware.container.ModuleContext;
 import org.universAAL.middleware.context.ContextEvent;
 import org.universAAL.middleware.context.ContextEventPattern;
 import org.universAAL.middleware.context.ContextPublisher;
+import org.universAAL.middleware.context.DefaultContextPublisher;
 import org.universAAL.middleware.context.owl.ContextProvider;
 import org.universAAL.middleware.context.owl.ContextProviderType;
 import org.universAAL.middleware.owl.MergedRestriction;
@@ -28,39 +29,74 @@ import org.universAAL.ontology.dependability.Fault;
 
 public class ReliabilityReasonerPublisher extends ContextPublisher {
 
-    protected ReliabilityReasonerPublisher(ModuleContext context,
-	    ContextProvider providerInfo) {
-	super(context, getProviderInfo());
+private DefaultContextPublisher myContextPublisher;
+
+    protected ReliabilityReasonerPublisher(ModuleContext context, ContextProvider providerInfo) {
+super(context, getProviderInfo());
 
     }
 
     protected ReliabilityReasonerPublisher(ModuleContext context) {
 	super(context, getProviderInfo());
-
+		ContextProvider myContextProvider = new ContextProvider("http://ontology.universAAL.org/Dependability.owl#Fault");
+ContextEventPattern myContextEventPattern = new ContextEventPattern();
+((ContextEventPattern) myContextEventPattern).addRestriction(MergedRestriction.getAllValuesRestriction(ContextEvent.PROP_RDF_SUBJECT, Fault.MY_URI));
+myContextProvider.setType(ContextProviderType.controller);
+ContextEventPattern[] myEvents = null;
+myContextProvider.setProvidedEvents(myEvents);
+//myContextProvider.setProvidedEvents(myContextEventPattern);
+myContextPublisher = new DefaultContextPublisher(context, myContextProvider);
     }
 
-    public void publishContextEvent(Fault myFault) {
-	ContextEvent myContextEvent = new ContextEvent(myFault,
-		Fault.PROP_FAULT_DECISION);
-	publish(myContextEvent);
-    }
+public void publishContextEvent(Fault myFault) {
+ContextEvent myContextEvent = new ContextEvent(myFault, Fault.PROP_FAULT_DECISION);
+myContextPublisher.publish(myContextEvent);
+}
 
     private static ContextProvider getProviderInfo() {
-	ContextProvider myContextProvider = new ContextProvider(
-		"http://ontology.universAAL.org/Dependability.owl#Fault");
-	ContextEventPattern myContextEventPattern = new ContextEventPattern();
-	((ContextEventPattern) myContextEventPattern)
-		.addRestriction(MergedRestriction.getAllValuesRestriction(
-			ContextEvent.PROP_RDF_SUBJECT, Fault.MY_URI));
-	myContextProvider.setType(ContextProviderType.reasoner);
-	ContextEventPattern[] myEvents = null;
-	myContextProvider.setProvidedEvents(myEvents);
-	return myContextProvider;
+	// TODO Auto-generated method stub
+	return null;
     }
 
     public void communicationChannelBroken() {
 	// TODO Auto-generated method stub
 
+    }
+
+    /* (non-Javadoc)
+     * @see org.universAAL.middleware.context.ContextPublisher#close()
+     */
+    @Override
+    public void close() {
+	// TODO Auto-generated method stub
+	super.close();
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#finalize()
+     */
+    @Override
+    protected void finalize() throws Throwable {
+	// TODO Auto-generated method stub
+	super.finalize();
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+	// TODO Auto-generated method stub
+	return super.hashCode();
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+	// TODO Auto-generated method stub
+	return super.toString();
     }
 
 }

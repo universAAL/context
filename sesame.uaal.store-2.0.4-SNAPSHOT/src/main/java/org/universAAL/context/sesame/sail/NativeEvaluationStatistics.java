c6213823d4b38faac5556ce724b5805be69a3ea0
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-01-20 15:16:11 +0000

    Automatic tag of sesame.uaal.store version: 2.0.4-SNAPSHOT

---
+++ sesame.uaal.store-2.0.4-SNAPSHOT/src/main/java/org/universAAL/context/sesame/sail/NativeEvaluationStatistics.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright Aduna (http://www.aduna-software.com/) (c) 2008.
+ *
+ * Licensed under the Aduna BSD-style license.
+ */
+package org.universAAL.context.sesame.sail;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.openrdf.model.Resource;
+import org.openrdf.model.URI;
+import org.openrdf.model.Value;
+import org.openrdf.query.algebra.StatementPattern;
+import org.openrdf.query.algebra.Var;
+import org.openrdf.query.algebra.evaluation.impl.EvaluationStatistics;
+
+/**
+ * @author Arjohn Kampman
+ * @author Enrico Minack
+ */
+class NativeEvaluationStatistics extends EvaluationStatistics {
+
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+	private final CardinalityNativeStore nativeStore;
+
+	public NativeEvaluationStatistics(CardinalityNativeStore nativeStore) {
+		this.nativeStore = nativeStore;
+	}
+
+	@Override
+	protected CardinalityCalculator createCardinalityCalculator() {
+		return new NativeCardinalityCalculator();
+	}
+
+	protected class NativeCardinalityCalculator extends CardinalityCalculator {
+
+		@Override
+		protected double getCardinality(StatementPattern sp) {
+			try {
+				Value subj = getConstantValue(sp.getSubjectVar());
+				if (!(subj instanceof Resource)) {
+					// can happen when a previous optimizer has inlined a comparison operator. 
+					// this can cause, for example, the subject variable to be equated to a literal value. 
+					// See SES-970 
+					subj = null;
+				}
+				Value pred = getConstantValue(sp.getPredicateVar());
+				if (!(pred instanceof URI)) {
+					//  can happen when a previous optimizer has inlined a comparison operator. See SES-970 
+					pred = null;
+				}
+				Value obj = getConstantValue(sp.getObjectVar());
+				Value context = getConstantValue(sp.getContextVar());
+				if (!(context instanceof Resource)) {
+					//  can happen when a previous optimizer has inlined a comparison operator. See SES-970 
+					context = null;
+				}
+				return nativeStore.cardinality((Resource)subj, (URI)pred, obj, (Resource)context);
+			}
+			catch (IOException e) {
+				log.error(
+						"Failed to estimate statement pattern cardinality, falling back to generic implementation",
+						e);
+				return super.getCardinality(sp);
+			}
+		}
+
+		protected Value getConstantValue(Var var) {
+			return (var != null) ? var.getValue() : null;
+		}
+	}
+}

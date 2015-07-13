2fe2484b310f60d45456d191ce730833f31e4a15
alfiva <alfiva@upvnet.upv.es>
2015-07-13 10:37:00 +0200

    Changed the hierarchy of "confidence" versions of backends
    In preparation of tenant-aware CHE, the "confidence" version of backends
    now extend the SesameBackendWithConfidence from CHE instead of the Crd
    ones in this module. This also avoids replicating the confidence logic

--- ctxt.che.module.cardinality/src/main/java/org/universAAL/context/che/database/impl/SesameBackendCrdCnf.java
+++ ctxt.che.module.cardinality/src/main/java/org/universAAL/context/che/database/impl/SesameBackendCrdCnf.java
@@ -1,5 +1,5 @@
 /*
-	Copyright 2014 ITACA-TSB, http://www.tsb.upv.es
+	Copyright 2015 ITACA-TSB, http://www.tsb.upv.es
 	Instituto Tecnologico de Aplicaciones de Comunicacion 
 	Avanzadas - Grupo Tecnologias para la Salud y el 
 	Bienestar (TSB)
@@ -21,23 +21,16 @@
  */
 package org.universAAL.context.che.database.impl;
 
-import java.io.IOException;
-import java.io.StringReader;
-import java.util.Iterator;
+import java.io.File;
 
-import org.openrdf.OpenRDFException;
-import org.openrdf.model.Statement;
-import org.openrdf.repository.RepositoryConnection;
-import org.openrdf.rio.RDFFormat;
-import org.openrdf.rio.helpers.StatementCollector;
-import org.openrdf.rio.turtle.TurtleParser;
+import org.openrdf.repository.sail.SailRepository;
+import org.openrdf.sail.inferencer.fc.ForwardChainingRDFSInferencer;
 import org.universAAL.context.che.Hub;
 import org.universAAL.context.che.Hub.Log;
-import org.universAAL.middleware.context.ContextEvent;
+import org.universAAL.context.sesame.sail.CardinalityNativeStore;
 
 /**
- * Extension of
- * {@link org.universAAL.context.che.database.impl.SesameBackendCrd} that
+ * Like a {@link org.universAAL.context.che.database.impl.SesameBackendCrd} that
  * interprets the confidence value of received events before storing them. If
  * the confidence is greater than the threshold passed to this class in the
  * constructor or <code>setThreshold</code>, the event will be stored unchanged
@@ -49,8 +42,8 @@ import org.universAAL.middleware.context.ContextEvent;
  * 
  * An "event1" with "subject2" "predicate3" and "object4" with enough confidence
  * will result in having the statements in the store: "event1" "hasSubject"
- * "subject2" "event1" "hasPredicate" "predicate3" "event1" "hasObject"
- * "object4" "subject2" "predicate3" "object4"
+ * "subject2", "event1" "hasPredicate" "predicate3", "event1" "hasObject"
+ * "object4", "subject2" "predicate3" "object4"
  * 
  * But if the confidence is below the threshold, the last reified statement is
  * not stored.
@@ -58,135 +51,43 @@ import org.universAAL.middleware.context.ContextEvent;
  * @author alfiva
  * 
  */
-public class SesameBackendCrdCnf extends SesameBackendCrd {
-    /**
-     * Logger.
-     */
-    private static final Log log = Hub.getLog(SesameBackendCrdClc2Cnf.class);
-    /**
-     * Confidence threshold.
-     */
-    private int threshold = 0;
+public class SesameBackendCrdCnf extends SesameBackendWithConfidence {
+	/**
+	 * Logger.
+	 */
+	private static final Log log = Hub.getLog(SesameBackendCrdCnf.class);
 
-    /**
-     * Default constructor.
-     */
-    public SesameBackendCrdCnf() {
-	super();
-	String conf = Hub.getProperties().getProperty("STORE.CONFIDENCE");
-	if (conf != null) {
-	    try {
-		setThreshold(Integer.parseInt(conf));
-	    } catch (Exception e) {
-		log.error("init", "Invalid confidence threshold. Using 0.", e);
-		setThreshold(0);
-	    }
-
-	} else {
-	    setThreshold(0);
-	}
-    }
-
-    /**
-     * Constructor with confidence.
-     * 
-     * @param confidence
-     *            0 to 100.
-     */
-    public SesameBackendCrdCnf(int confidence) {
-	super();
-	this.setThreshold(confidence);
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * org.universAAL.context.che.database.impl.SesameBackend#storeEvent(org
-     * .universAAL.middleware.context.ContextEvent)
-     */
-    @Override
-    synchronized public void storeEvent(ContextEvent e) {
-	try {
-	    RepositoryConnection con = myRepository.getConnection();
-	    try {
-		log.debug("storeEvent",
-			"Adding event to store, if enough confidence");
-		Integer conf = e.getConfidence();
-		if (conf != null) {
-		    if (conf.intValue() < threshold) {
-			TurtleParser sesameParser = new TurtleParser();
-			StatementCollector stHandler = new StatementCollector();
-			sesameParser.setRDFHandler(stHandler);
-			sesameParser.parse(
-				new StringReader(uAALParser.serialize(e)),
-				e.getURI());
-			Iterator<Statement> sts = stHandler.getStatements()
-				.iterator();
-			while (sts.hasNext()) {
-			    Statement st = sts.next();
-			    if (st.getSubject().stringValue()
-				    .equals(e.getURI())) {
-				con.add(st);
-				// store only stmts having event as subject
-			    }
+	@Override
+	public void connect() {
+		String dataPath = Hub.getProperties().getProperty("STORE.LOCATION");
+		boolean encrypt = Boolean.parseBoolean(Hub.getProperties().getProperty(
+				"STORE.ENCRYPT"));
+		// I use C:\Proyectos\UNIVERSAAL\ContextStore\Stores\SAIL_FCRDFS_Native
+		if (dataPath != null) {
+			File dataDir = new File(dataPath);
+			String indexes = "spoc,posc,cosp"; // TODO: Change indexes
+			log.info("CHe connects to {} ", dataDir.toString());
+			// TODO: Evaluate the inference, and study other reasoners, if any
+			try {
+				myRepository = new SailRepository(
+						new ForwardChainingRDFSInferencer(
+								new CardinalityNativeStore(dataDir, indexes,
+										encrypt)));
+				myRepository.initialize();
+				if (Boolean.parseBoolean(Hub.getProperties().getProperty(
+						"STORE.PRELOAD"))) {
+					this.populate();
+				}
+			} catch (Exception e) {
+				log.error("connect",
+						"Exception trying to initilaize the store: {} ", e);
+				e.printStackTrace();
 			}
-			log.info("storeEvent",
-				"CHe: Stored a Context Event with "
-					+ "low Confidence: Not reified.");
-		    } else {
-			con.add(new StringReader(uAALParser.serialize(e)),
-				e.getURI(), RDFFormat.TURTLE);
-			log.info("storeEvent", "CHe: Stored a Context Event"
-				+ " with high Confidence");
-		    }
-		} else { // TODO: What to do if events have no confidence?
-		    con.add(new StringReader(uAALParser.serialize(e)),
-			    e.getURI(), RDFFormat.TURTLE);
-		    log.info("storeEvent",
-			    "CHe: Stored a Context Event without Confidence");
+		} else {
+			log.error("connect", "No location specified for the store. "
+					+ "Add and specify the configuration parameter "
+					+ "STORE.LOCATION to the configuration file of the CHE "
+					+ "pointing to a valid folder path.");
 		}
-		log.debug("storeEvent", "Successfully added event to store");
-	    } catch (IOException exc) {
-		log.error("storeEvent",
-			"Error trying to add event to the store. "
-				+ "In older versions this usually"
-				+ " happened because of the underlying"
-				+ " connection closing due to inactivity,"
-				+ " but now it is because: {}", exc);
-		exc.printStackTrace();
-	    } finally {
-		con.close();
-	    }
-	} catch (OpenRDFException exc) {
-	    log.error("storeEvent",
-		    "Error trying to get connection to store: {}", exc);
-	    exc.printStackTrace();
 	}
-    }
-
-    /**
-     * Get confidence threshold.
-     * 
-     * @return Confidence.
-     */
-    public int getThreshold() {
-	return threshold;
-    }
-
-    /**
-     * Set confidence threshold.
-     * 
-     * @param threshold
-     *            Confidence.
-     */
-    public void setThreshold(int thr) {
-	if (threshold < 100) {
-	    this.threshold = thr;
-	} else {
-	    this.threshold = 100;
-	}
-
-    }
-
 }
